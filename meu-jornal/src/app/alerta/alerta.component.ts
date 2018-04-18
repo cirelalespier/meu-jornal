e62c7c7f88c3be95d95ceb56c6276dad78e77046
cirelalespier <lespier.cirela@gmail.com>
2018-04-18 20:44:42 +0100

    teste

---
+++ meu-jornal/src/app/alerta/alerta.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alerta',
+  templateUrl: './alerta.component.html',
+  styleUrls: ['./alerta.component.css']
+})
+export class AlertaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public MostrarAlerta() {
+    alert('Estou gostando do curso!');
+  }
+
+}
--- meu-jornal/src/app/noticia/noticia.component.ts
+++ meu-jornal/src/app/noticia/noticia.component.ts
@@ -9,9 +9,19 @@ export class NoticiaComponent implements OnInit {
 
   noticias: string[] = ["Noticia1", "Noticia2","Noticia3","Noticia4"];
 
+  verdadeiro: boolean = true;
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public Noticia(){
+    this.verdadeiro = false;
+  }
+
+  public Voltar(){
+    this.verdadeiro =  true;
   }
 
 }
